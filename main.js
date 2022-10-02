f32060ffabe6b4dcc07345b22f21365b03d23dd2
mqashoo77 <mohammadqashoo77@gmail.com>
2022-10-03 00:26:37 +0300

    formating

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
-const fs = require('fs');
-const readline = require('readline');
-const filaName = 'users.csv'
+const fs = require("fs");
+const readline = require("readline");
+const filaName = "users.csv";
 const fileStream = fs.createReadStream(filaName);
-
 const interfaceReader = readline.createInterface({
-    input: fileStream
+  input: fileStream,
 });
 
-
-async function readFromFile(){
-    users = [] ;
-    for await (const line of interfaceReader) {
-       
-       const temp = line.split(",")
-       users.push(temp)
-    }
-    return users;
+async function readFromFile() {
+  users = [];
+  for await (const line of interfaceReader) {
+    const temp = line.split(",");
+    users.push(temp);
+  }
+  return users;
 }
 
-async function writeOnFile(users){
-    let csv = "";
-    for (let index = 1; index < users.length; index++) {
-        csv += users[index]
-        csv+="\n"
-    }
-    fs.writeFileSync("write.csv", csv);
-    return users
-
+async function writeOnFile(users) {
+  let csv = "";
+  for (let index = 1; index < users.length; index++) {
+    csv += users[index];
+    csv += "\n";
+  }
+  fs.writeFileSync("write.csv", csv);
+  return users;
 }
 
-async function writeOnJsonFile(users){
-    let json = "{\n\t";
-    let Info = [users[0][0],users[0][1],users[0][2],users[0][3],users[0][4]]
-    let numberOfUser = users.length-1
-    let i = 0
-    for ( ;numberOfUser !==0; ){
-        json += `"${i+1}":{\n\t\t`
-        for(let values = 0; values < Info.length ; values++){
-            json += `"${Info[values]}": "${users[i+1][values]}"`
-            json = (values !== Info.length- 1) ?  json+=",\n\t\t" : json 
-        }
-        numberOfUser--
-        i++
-        json= (numberOfUser !== 0) ? json += "\n\t},\n\t" : json += "\n\t}\n\t"
+async function writeOnJsonFile(users) {
+  let json = "{\n\t";
+  let Info = [users[0][0], users[0][1], users[0][2], users[0][3], users[0][4]];
+  let numberOfUser = users.length - 1;
+  let i = 0;
+  for (; numberOfUser !== 0; ) {
+    json += `"${i + 1}":{\n\t\t`;
+    for (let values = 0; values < Info.length; values++) {
+      json += `"${Info[values]}": "${users[i + 1][values]}"`;
+      json = values !== Info.length - 1 ? (json += ",\n\t\t") : json;
     }
-    json += '\n}'
-    fs.writeFileSync("write.json", json);
+    numberOfUser--;
+    i++;
+    json = numberOfUser !== 0 ? (json += "\n\t},\n\t") : (json += "\n\t}\n\t");
+  }
+  json += "\n}";
+  fs.writeFileSync("write.json", json);
 }
 
-readFromFile().then(writeOnFile).then(writeOnJsonFile)
+readFromFile().then(writeOnFile).then(writeOnJsonFile);
